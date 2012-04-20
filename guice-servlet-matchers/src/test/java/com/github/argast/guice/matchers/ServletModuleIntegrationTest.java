400a7139a73691faeae750e6142a777d6e139a13
Pawel Poltorak <argast1@gmail.com>
2012-04-20 19:01:38 +0100

    Added tests for regex pattern specification

--- guice-servlet-matchers/src/test/java/com/github/argast/guice/matchers/ServletModuleIntegrationTest.java
+++ guice-servlet-matchers/src/test/java/com/github/argast/guice/matchers/ServletModuleIntegrationTest.java
@@ -1,5 +1,9 @@
 package com.github.argast.guice.matchers;
 
+import static com.github.argast.guice.matchers.GuiceServletMatchers.assertFilter;
+import static com.github.argast.guice.matchers.GuiceServletMatchers.assertServlet;
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Enumeration;
@@ -22,8 +26,6 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceFilter;
 import com.google.inject.servlet.ServletModule;
-import static com.github.argast.guice.matchers.GuiceServletMatchers.*; 
-import static org.junit.Assert.*;
 
 public class ServletModuleIntegrationTest {
 
@@ -32,25 +34,38 @@ public class ServletModuleIntegrationTest {
 		put("key2", "value2");
 	}};
 	
-	private static class FakeFilter implements Filter {
+	private static class UriFilter implements Filter {
 		public void destroy() {}
 		public void doFilter(ServletRequest request, ServletResponse response,
 				FilterChain chain) throws IOException, ServletException {}
 		public void init(FilterConfig filterConfig) throws ServletException {}
 	}
 	
+	private static class RegexFilter implements Filter {
+		public void destroy() {}
+		public void doFilter(ServletRequest request, ServletResponse response,
+				FilterChain chain) throws IOException, ServletException {}
+		public void init(FilterConfig filterConfig) throws ServletException {}
+	}
+	
+	
 	@SuppressWarnings("serial")
-	private static class FakeServlet extends HttpServlet {}
+	private static class UriServlet extends HttpServlet {}
+	private static class RegexServlet extends HttpServlet {}
 	
 	private Injector injector = Guice.createInjector(new ServletModule() {
 		protected void configureServlets() {
-			bind(FakeServlet.class).asEagerSingleton();
-			serve("/test/*", "/second/test/*").with(FakeServlet.class, PARAMS);
+			bind(UriServlet.class).asEagerSingleton();
+			serve("/test/*", "/second/test/*").with(UriServlet.class, PARAMS);
 			
-			bind(FakeFilter.class).asEagerSingleton();
-			filter("/to/filter/*").through(FakeFilter.class, Collections.singletonMap("filterKey", "filterValue"));
+			bind(UriFilter.class).asEagerSingleton();
+			filter("/to/filter/*").through(UriFilter.class, Collections.singletonMap("filterKey", "filterValue"));
+			
+			bind(RegexServlet.class).asEagerSingleton();
+			serveRegex("/regex/(test|impl)/[abc]*/.*.html").with(RegexServlet.class);
 		};
 	});
+	
 	
 	@Before
 	public void setUp() throws Exception {
@@ -60,54 +75,64 @@ public class ServletModuleIntegrationTest {
 	
 	@Test
 	public void testThatUriIsServed() throws Exception {
-		assertServlet(FakeServlet.class).serves("/test/uri/*").on(injector);
+		assertServlet(UriServlet.class).serves("/test/uri/*").on(injector);
 	}
 	
 	@Test
 	public void testThatSecondUriIsServed() throws Exception {
-		assertServlet(FakeServlet.class).serves("/second/test/uri/*").on(injector);
+		assertServlet(UriServlet.class).serves("/second/test/uri/*").on(injector);
 	}
 	
 	@Test
 	public void testThatPatternIsServed() throws Exception {
-		assertServlet(FakeServlet.class).servesPattern("/test/*").on(injector);
+		assertServlet(UriServlet.class).servesPattern("/test/*").on(injector);
 	}
 
 	@Test
 	public void testThatSecondPatternIsServed() throws Exception {
-		assertServlet(FakeServlet.class).servesPattern("/second/test/*").on(injector);
+		assertServlet(UriServlet.class).servesPattern("/second/test/*").on(injector);
 	}
 	
 	@Test
 	public void testThatUriIsFiltered() throws Exception {
-		assertFilter(FakeFilter.class).filters("/to/filter/*").on(injector);
+		assertFilter(UriFilter.class).filters("/to/filter/*").on(injector);
 	}
 	
 	@Test
 	public void testThatServletIsInitializedWithParam() throws Exception {
-		assertServlet(FakeServlet.class).hasInitParameter("key1", "value1").on(injector);
+		assertServlet(UriServlet.class).hasInitParameter("key1", "value1").on(injector);
 	}
 	
 	@Test
 	public void testThatServletIsInitializedWithParams() throws Exception {
-		assertServlet(FakeServlet.class).hasInitParameters(PARAMS).on(injector);
+		assertServlet(UriServlet.class).hasInitParameters(PARAMS).on(injector);
 	}
 
 	@Test(expected = AssertionError.class)
 	public void testThatServletIsNotInitializedWithParams() throws Exception {
-		assertServlet(FakeServlet.class).hasInitParameters(Collections.singletonMap("key1", "value1")).on(injector);
+		assertServlet(UriServlet.class).hasInitParameters(Collections.singletonMap("key1", "value1")).on(injector);
 	}
 	
 	@Test
 	public void testThatFilterIsInitializedWithParameter() throws Exception {
-		assertFilter(FakeFilter.class).hasInitParameter("filterKey", "filterValue").on(injector);
+		assertFilter(UriFilter.class).hasInitParameter("filterKey", "filterValue").on(injector);
 	}
 	
 	@Test(expected = AssertionError.class)
-	public void testThatAssertionErrorIsThrownIfTestChecksForUnusedParameter() throws Exception {
-		assertFilter(FakeFilter.class).hasInitParameter("noSuchKey", "noSuchValue").on(injector);
+	public void testThatAssertionErrorIsThrownIfTestChecksForNonExistingParameter() throws Exception {
+		assertFilter(UriFilter.class).hasInitParameter("noSuchKey", "noSuchValue").on(injector);
 	}
 	
+	@Test
+	public void testThatServletServesRegex() throws Exception {
+		assertServlet(RegexServlet.class).serves("/regex/test/abcacb/index.html").on(injector);
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testThatAssertionErrorIsThrownWhenServletDoesNotServeRegex() throws Exception {
+		assertServlet(RegexServlet.class).serves("/regex/test/abc/index.xml").on(injector);
+	}
+
 
 	private final class DummyFilterConfig implements FilterConfig {
 		public ServletContext getServletContext() {
